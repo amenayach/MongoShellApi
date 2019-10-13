1e2c783f03680920e6bfd04238f9f714953800fe
Amen Ayach <aayach@Netways.com>
2019-10-13 03:42:34 +0300

    styling and localstorage for last inputs

--- src/MongoShellApi/wwwroot/app.js
+++ src/MongoShellApi/wwwroot/app.js
@@ -4,9 +4,25 @@
         var tbConnection = this.document.getElementById('tbConnection');
         var btnConnect = this.document.getElementById('btnConnect');
         var btnExec = this.document.getElementById('btnExec');
+        var btnHelp= this.document.getElementById('btnHelp');
         var tbResult = this.document.getElementById('tbResult');
         var tbQuery = this.document.getElementById('tbQuery');
         var imgLoader = this.document.getElementById('imgLoader');
+
+        var localStorageKey = 'data';
+        var localStorageValue = {
+            connection: 'mongodb://localhost:27017/lookupdb',
+            query: `{
+  "find": "country",
+  "filter": {}
+}`
+        };
+
+        function init() {
+            localStorageValue = ls.get(localStorageKey, localStorageValue);
+            tbConnection.value = localStorageValue.connection;
+            tbQuery.value = localStorageValue.query;
+        }
 
         function wait(doWait) {
             if (doWait) {
@@ -32,6 +48,28 @@
                 wait(false);
             });
         });
+
+        btnHelp.addEventListener('click', function () {
+            window.open('https://docs.mongodb.com/manual/reference/command/nav-crud/', '_blank');
+        });
+
+        tbConnection.addEventListener('keyup', function () {
+            localStorageValue.connection = tbConnection.value;
+            ls.set(localStorageKey, localStorageValue);
+        });
+
+        tbQuery.addEventListener('keyup', function () {
+            localStorageValue.query = tbQuery.value;
+            ls.set(localStorageKey, localStorageValue);
+        });
+
+        this.document.addEventListener('keydown', function (ev) {
+            if (ev.ctrlKey && ev.keyCode === 13) {
+                btnExec.click();
+            }
+        });
+
+        init();
     });
 
 })();
---
+++ src/MongoShellApi/wwwroot/storage-service.js
@@ -0,0 +1,19 @@
+﻿(function () {
+    var ls = {};
+
+    ls.get = function (key, defaultValue) {
+        var value = window.localStorage.getItem(key);
+
+        if (!defaultValue) {
+            defaultValue = {};
+        }
+
+        return value ? JSON.parse(value) : defaultValue;
+    };
+
+    ls.set = function (key, value) {
+        window.localStorage.setItem(key, JSON.stringify(value));
+    };
+
+    window.ls = ls;
+})();
